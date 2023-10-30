a26b6d5f6b3c8dadf7e430af58b5f692f70444d0
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2023-10-30 15:19:55 +0100

    footer update

--- src/script/nav-footer.js
+++ src/script/nav-footer.js
@@ -1,23 +1,50 @@
 // variable takes the footer html code
-const footerHtml = `<div class="container mx-auto flex items-center justify-between">
-  <div>
-    <p>&copy; 2023 Your Company</p>
+const footerHtml = `<div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+<div class="md:flex md:justify-between">
+  <div class="mb-6 md:mb-0">
+      <a href="https://flowbite.com/" class="flex items-center">
+          <span class="self-center text-2xl font whitespace-nowrap dark:text-white text-black text-3xl font-bold">SwiftBooking</span>
+      </a>
   </div>
-  <div class="flex space-x-4">
-    <a href="#" class="text-white hover:text-gray-500">
-      <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 10v8a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-8m11 0l-3-3m0 0l-3 3m3-3V2m0 11H2m10 0v8m0 0l3-3"></path>
-      </svg>
-    </a>
-    <a href="#" class="text-white hover:text-gray-500">
-      <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 4h2a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3h-2m-4-2l-6 6m0 0l-6-6m6 6V4"></path>
-      </svg>
-    </a>
-    <!-- Add more social icons as needed -->
+  <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+      <div>
+          <h2 class="mb-3 text-sm font-semibold text-gray-900 uppercase dark:text-white">Services</h2>
+          <ul class="text-gray-500 text-black">
+              <li class="mb-2">
+                  <a href="https://flowbite.com/" class="hover:underline">Restaurant Reservation</a>
+              </li>
+              <li>
+                  <a href="https://tailwindcss.com/" class="hover:underline">Event Venue Reservation</a>
+              </li>
+          </ul>
+      </div>
+      <div  class="ml-20">
+          <h2 class="mb-3 text-sm font-semibold text-gray-900 uppercase dark:text-white">Contact</h2>
+          <ul class="text-gray-500 text-black ">
+              <li class="mb-4">
+                  <a href="https://github.com/themesberg/flowbite" class="hover:underline ">Help and Support</a>
+              </li>
+          </ul>
+      </div>
+      
   </div>
 </div>
-`;
+<div class="sm:flex sm:items-center sm:justify-between">
+  <div class="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
+      <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+          <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAARtJREFUSEvt1y9LBUEUBfDf+wIKNhEUxGBRBKvBP8FiNQgifgej1WZSzEaDn8CmIFZNBotBES0GMZmUgVkYFp67D2bZx+q0geUczjn33rnb09LptcRrqIgnMZ3ZiUc8pZhlxeN4IbsT35jAa0FeJl7GZWa1BdwKrjpD/IVD3OMtqprDceJedsWhJtbwUIpoCddNEq/2qYtGiT8xkqiax1HsilEsNKX4FosJ+Dk2+3RF1oxvECwtzgXWO0f8jJ2o6gN3pYzH4n0DezkzDm0zW2O6HWC/DeIzbOUkfsdJBAwvzmkCvo2ZeN/FVE7i1OW/UdX/irs/uRrJeNBlr247VS57Qc0g6+1vszp1pnK9rTGC83wyVH8SeSRVoPwAKWhhH7pX+W8AAAAASUVORK5CYII="/>
+      </a>
+      <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+          <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAmpJREFUSEvl1knozVEUwPHPXxaKMiwMCSGhEFmYx9iQqVhYkaEsLNggw8aQMmXBgkgpKzamjYV5LBkyZCxKyJDEwoZ09Pvr9et3f+89T/2Vu3mv+zvnfO859wy3SQutphbi+ufB3dAPrapE6Due4U21SJZ53BXbMR0dqxnKff+E01iVOkQKPAtH0LZOYF78K+bjVP5DEbgLHqN9g9Bm9Y/oj/j9vYrAB7GoAehztEavChsHsLQMHApxP+3+EHwHwzLdexiU/f+CDvjRbDfvcU+8rAINj7bhCUJ+JOYgkrESfBtDK2x1xvsUeDhuloBXYyeibCpX5EPsL8ZTfMPgnEx4/yAFnoDzCfBy7MUAzMPYTO4SjmYJuQsrE/oRmRsp8FScKVC8koEiO/dgSk4mdOJgcU13s8PlzYzGtXrBC3AY67A54dFabMUmrC+QGYOr9YIjrOF1dKNpCfAJROOZm4U+LzYecS2/Vj6rJ+FsgeHJOJd1oGihRes4ZpeAJ+JCChwfA5Bfy7APa7JwFoEj46PMtiDCnl/jcLlecGRjZGUf7MaMnNXwdgU+4BG6F4BH4XoKHKe6mAhlczn1zsI5IpMLY8fwAvvzrbHCVvSIWynwkKwcEmwRzh2VrS8TrGwgKd1oKPdT4JhMb1Oa2X5MriibqNlomVGfMxGPhbIV4X+dAsf+q8QdVbFb+vkzOpUNidDegI2NUAp0Y9QuqdwvmscxGh9mb6y/wQ9v+9byEAhYD0SJNM/WRg4Qj4pDeQNlj7022TRaWMPrMm83Bv5JxLQqLM9a3tWR6QPrdDmu6l2ZTi3gOpm1if9/4J9oyHEfOQrU/wAAAABJRU5ErkJggg=="/>
+      </a>
+      <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+          <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAV9JREFUSEvt179LV2EUx/GX0BAIIWhSSA02GIS4uUX5D1R/hlRQzToJOiqC4uTmHyCEm6Dg1CyBoAQKoaWLo4MoB54Lj1cJv3zv917BzvY8l3Pe93zOeX51aci6GuK6U+DnGKxYiV84yGOWM36K31SuxAUGcFjAy+C32Kg42yLcGDZbAe/iGR62+UMtgd/hO7qxhjdtwG8N/oMnGWgcS3WAz9GL0wSbxbc6wMFYxzJe4TN66gK3wbnmeusah+eHTOovabySVIjvo4gGnMMW/uI1phBLM7eWwI9xkrwX8AnTmExzUYI9nJUgD7CNl9l8peAi7jCOcJyBPmKxk+BVvEesghH8TLAow49OgR9lPRCMCcwkWBw2+50Cv0g1LuLP42sa9JWkb6nG/ZnzTc01hJ0sq9jZorZh0ZjR5YX9B19ZgeXz+P5J3dheXRu4scteZNjI9bZKaf8Z6069JGrJ+hJQKIAfFGduPAAAAABJRU5ErkJggg=="/>
+      </a>
+  </div>
+  <span class="mt-14 text-sm text-gray-500 sm:text-center text-black">© 2023 <a href="https://flowbite.com/" class="hover:underline">SwiftBooking</a>. All Rights Reserved.
+  </span>
+</div>
+</div>`;
 
 // variable takes the header html code
 const headerHtml = `<div class="fixed top-0 left-0 right-0 z-10 bg-transparent p-4">
